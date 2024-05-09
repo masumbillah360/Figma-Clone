c337e106b45ad02e27a0c92fd99e555f0b7088b4
Masum Billah <masumbillah6584@gmail.com>
2024-05-09 23:35:00 +0600

    updated code

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { CollaborativeApp } from "./CollaborativeApp";
+import { Room } from "./Room";
+
+export default function Page() {
+    return (
+        <Room>
+            <CollaborativeApp />
+        </Room>
+    );
+}
